7d7efcabe72c5ef3225a333663e02973a23c237b
Robert Filipe <mcfipe10010@gmail.com>
2026-05-28 16:46:13 -0300

    Implementação da entidade Inscricoes

--- src/main/java/trabalho/apiReade/model/Inscricoes.java
+++ src/main/java/trabalho/apiReade/model/Inscricoes.java
@@ -1,4 +1,57 @@
 package trabalho.apiReade.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Inscricoes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate dataInscricao;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "evento_id")
+    private Evento evento;
+
+    public Inscricoes() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataInscricao() {
+        return dataInscricao;
+    }
+
+    public void setDataInscricao(LocalDate dataInscricao) {
+        this.dataInscricao = dataInscricao;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
 }
