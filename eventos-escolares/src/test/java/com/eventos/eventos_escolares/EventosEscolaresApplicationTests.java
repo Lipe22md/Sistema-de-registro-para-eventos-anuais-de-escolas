215cf82b02926e0d06b7665c9590972fbabcb891
Luca Melo <144393351+LucasMelo725@users.noreply.github.com>
2026-05-31 21:24:51 -0300

    Add files via upload

---
+++ eventos-escolares/src/test/java/com/eventos/eventos_escolares/EventosEscolaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.eventos_escolares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosEscolaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
